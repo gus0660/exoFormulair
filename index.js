dba6a376c1d405457a0bbe264ec458f4bb5c4a95
gus0660 <guillaumeroch3@gmail.com>
2024-02-12 11:06:53 +0100

    début du js

---
+++ index.js
@@ -0,0 +1,6 @@
+const inputNom = document.querySelector("#inputnom");
+const boutonSubmit = document.querySelector("#submit");
+
+boutonSubmit.addEventListener('click', () => {
+    if (inputNom.value = "" ),
+})
