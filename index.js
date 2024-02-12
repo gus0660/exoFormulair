a67e2239cd14c146c132a665726edbee981bf340
gus0660 <guillaumeroch3@gmail.com>
2024-02-12 11:35:38 +0100

    commentaires

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const boutonSubmit = document.querySelector("#form");
 
 boutonSubmit.addEventListener('submit', (e) => {
     e.preventDefault()
-    console.log(inputNom.value)
+    // console.log(inputNom.value)
     if (inputNom.value == "") {
-        console.log("marche pô")
+        // console.log("marche pô")
     }
 })
