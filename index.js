b999a0bf37c7e471dc8c5e26d4d0563319c37f58
gus0660 <guillaumeroch3@gmail.com>
2024-02-12 12:24:56 +0100

    function isBetween

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const boutonSubmit = document.querySelector("#form");
 boutonSubmit.addEventListener('submit', (e) => {
     e.preventDefault()
     // console.log(inputNom.value)
-    if (inputNom.value == "" || inputNom.value.length > 3 || inputNom.value.length < 25 || inputNom.value != "afpa") {
+    if (inputNom.value == ""||inputNom.value.length < 3||inputNom.value.length > 25||inputNom.value === "afpa"||inputNom.value === "root") {
         console.log("marche pô")
     }
 })
+function isBetween(length,min,max) {
+
+}
