c8293e76d0ae9e3e38d05eb45df5fbeb4e80fe0b
gus0660 <guillaumeroch3@gmail.com>
2024-02-12 16:06:17 +0100

    début de la regExp

--- index.js
+++ index.js
@@ -4,17 +4,21 @@ const boutonSubmit = document.querySelector("#form");
 boutonSubmit.addEventListener('submit', (e) => {
     e.preventDefault()
     // console.log(inputNom.value)
-    isBetween();
+    const min = 3;
+    const max = 25;
+    if (!isBetween(inputNom.value.length,min, max)) {
+        console.log("marche pô")
+    };
         // console.log("marche pô")
     
 })
 
 function isBetween(length,min,max) {
-    const min = 3;
-    const max = 25;
+    
     if (length<min||length>max) {
         return false
     }else{
         return true
     }
 }
+// const regex = new RegExp('["afpa", "root", "deus"]')
